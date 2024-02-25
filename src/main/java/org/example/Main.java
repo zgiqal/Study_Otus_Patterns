634f7509c7b2225bc70f95428ecb5166ac5328a4
zgiqal <zgiqal@yandex.ru>
2024-02-25 14:30:33 +0300

    Доработка тестов и функциональности
    update gitignore
    
    update gitignore

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+    public static final double epsilon = 1e-7;
+
     public static void main(String[] args) {
 
         Scanner paramsInput = new Scanner(System.in);
@@ -21,6 +24,29 @@ public class Main {
     }
 
     static double[] solve(double a, double b, double c) {
-        return null;
+        if ((Math.abs(a - epsilon) < epsilon)
+                || !Double.isFinite(a)
+                || !Double.isFinite(b)
+                || !Double.isFinite(c)
+        ) {
+            throw new IllegalArgumentException();
+        }
+
+        double discriminant = b*b - 4*a*c;
+        if (discriminant < 0) {
+            return new double[]{};
+        }
+        if (discriminant < epsilon) {
+            double root = getRoot(a, b, null);
+            return new double[]{root, root};
+        } else {
+            double sqrt = Math.sqrt(discriminant);
+            return new double[]{getRoot(a, b, sqrt), getRoot(a, b, -sqrt)};
+
+        }
+    }
+
+    private static double getRoot(double a, double b, Double d) {
+        return d == null ? -b / (2*a) : (-b + d) / (2*a);
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,13 +1,55 @@
 package org.example;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.example.common.ArraySource;
+import org.example.common.ArraySources;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.example.Main.epsilon;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
 
+
+    @ParameterizedTest
+    @ArraySources(arrays = {
+            @ArraySource(inputs = {1, 0, 1}),
+//            @ArraySource(inputs = {1, 0, -1}, outputs = {1, -1}),
+//            @ArraySource(inputs = {1 + epsilon, 2 + epsilon/4, 1}, outputs = {-1, -1}),
+    })
+    void givenSquareCoefficients_whenSolve_thenExpectedRoots(double[] inputs, double[] roots) {
+        double[] results = Main.solve(inputs[0], inputs[1], inputs[2]);
+        Set<Double> expected = new HashSet<>(Arrays.asList(ArrayUtils.toObject(roots)));
+        Set<Double> actual = new HashSet<>(Arrays.asList(ArrayUtils.toObject(results)));
+        assertEquals(roots.length, results.length);
+        expected.forEach(r -> assertTrue(actual.stream().anyMatch(a -> Math.abs(a-r) < epsilon)));
+    }
+
     @Test
-    void givenCoefficients101_whenSolve_thenEmptyRoots() {
-        assertEquals(new double[0], Main.solve(1, 0, 1));
+    void givenNullFirst_whenSolve_thenThrowException() {
+        assertThrows(IllegalArgumentException.class, ()-> Main.solve(epsilon, 1,1));
+        assertDoesNotThrow(() -> Main.solve(epsilon * 10, 1, 1));
+    }
+
+    @ParameterizedTest
+    @ArraySources(arrays = {
+            @ArraySource(inputs = {Double.NaN, 1, 1}),
+            @ArraySource(inputs = {1, Double.NaN, 1}),
+            @ArraySource(inputs = {1, 1, Double.NaN}),
+            @ArraySource(inputs = {Double.POSITIVE_INFINITY, 1, 1}),
+            @ArraySource(inputs = {1, Double.POSITIVE_INFINITY, 1}),
+            @ArraySource(inputs = {1, 1, Double.POSITIVE_INFINITY}),
+            @ArraySource(inputs = {Double.NEGATIVE_INFINITY, 1, 1}),
+            @ArraySource(inputs = {1, Double.NEGATIVE_INFINITY, 1}),
+            @ArraySource(inputs = {1, 1, Double.NEGATIVE_INFINITY}),
+    })
+    void givenSpecial_whenSolve_thenThrowException() {
+        assertThrows(IllegalArgumentException.class, ()-> Main.solve(epsilon, 1,1));
     }
 }
